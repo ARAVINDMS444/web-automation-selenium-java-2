02c686965dcc4d8da3650ca1bbce1c3df6dadf97
Aravind MS <aravind.ms4444@gmail.com>
2025-07-29 21:51:20 +0530

    Commit 24

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -3,7 +3,9 @@ package utils;
 public class Constants {
 
   public static String baseUrl = "https://the-internet.herokuapp.com/login";
-  public static final String TEST_DATA_PATH =
+  public static final String XLS_PATH =
+      System.getProperty("user.dir") + "/test-artifacts/testData/LoginData.xls";
+  public static final String XLSX_PATH =
       System.getProperty("user.dir") + "/test-artifacts/testData/LoginData.xlsx";
   public static final String LOGIN_SHEET = "LoginCredentials";
 }
--- src/test/java/smokeTests/PracticeTest1.java
+++ src/test/java/smokeTests/PracticeTest1.java
@@ -25,15 +25,21 @@ public class PracticeTest1 extends BaseTest {
   @Test(priority = 3)
   public void verifyInvalidLogin2() throws InterruptedException, IOException {
     LoginPage loginPage = new LoginPage(getDriver());
-    String username =
-        ExcelReader.getCellValue(Constants.TEST_DATA_PATH, Constants.LOGIN_SHEET, 1, 0);
-    String password =
-        ExcelReader.getCellValue(Constants.TEST_DATA_PATH, Constants.LOGIN_SHEET, 1, 1);
+    String username = ExcelReader.getCellValue(Constants.XLS_PATH, Constants.LOGIN_SHEET, 1, 0);
+    String password = ExcelReader.getCellValue(Constants.XLS_PATH, Constants.LOGIN_SHEET, 1, 1);
     loginPage.verifyInvalidLogin(username, password);
   }
 
   @Test(priority = 4)
   public void verifyInvalidLogin3() throws InterruptedException, IOException {
+    LoginPage loginPage = new LoginPage(getDriver());
+    String username = ExcelReader.getCellValue(Constants.XLSX_PATH, Constants.LOGIN_SHEET, 1, 0);
+    String password = ExcelReader.getCellValue(Constants.XLSX_PATH, Constants.LOGIN_SHEET, 1, 1);
+    loginPage.verifyInvalidLogin(username, password);
+  }
+
+  @Test(priority = 5)
+  public void verifyInvalidLogin4() throws InterruptedException, IOException {
     LoginPage loginPage = new LoginPage(getDriver());
     String username = ConfigReader.get("username");
     String password = ConfigReader.get("password");
